e53081b1503c9cb8977bd0483075cae522e1fa90
Jan Tourlamain <jan.tourlamain@outlook.com>
2016-11-04 22:53:33 +0100

    added apikeys file

---
+++ src/Tobania.Xam.Tobit/Config/ApiKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Tobania.Xam.Tobit.Config
+{
+	public static class ApiKeys
+	{
+		public const string ClientId = "<YOUR-CLIENT-ID>";
+		public const string ClientSecret = "<YOUR-CLIENT-SECRET>";
+		public const string RedirectUrl = "<YOUR-REDIRECT-URL>";
+		public const string Scope = "user repo";
+		public const string AuthorizeUrl = "https://github.com/login/oauth/authorize";
+		public const string AccessTokenUrl = "https://github.com/login/oauth/access_token";
+		public const string GitHubApi = "https://api.github.com";
+		public const string AccessToken = "access_token";
+	}
+}
