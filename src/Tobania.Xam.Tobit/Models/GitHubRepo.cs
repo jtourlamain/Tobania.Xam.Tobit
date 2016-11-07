3cbd7cd627294f5dd0b770b0c370b3182af103de
Jan Tourlamain <jan.tourlamain@outlook.com>
2016-11-07 21:34:44 +0100

    before service requests2

--- src/Tobania.Xam.Tobit/Models/GitHubRepo.cs
+++ src/Tobania.Xam.Tobit/Models/GitHubRepo.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Newtonsoft.Json;
+
 namespace Tobania.Xam.Tobit.Models
 {
 	public class GitHubRepo
 	{
-		public GitHubRepo()
-		{
-		}
+		public int Id { get; set; }
+		public string Name { get; set; }
+		[JsonProperty("full_name")]
+		public string FullName { get; set; }
+		public string Url { get; set; }
+		[JsonProperty("html_url")]
+		public string HtmlUrl { get; set; }
 	}
 }
